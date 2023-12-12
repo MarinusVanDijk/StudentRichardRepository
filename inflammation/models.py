56de91425c4e51c49e4a7382fc64ebfc985b6f6a
Richard Dijk <richard.van.dijk@snelnet.net>
2023-12-12 12:20:26 +0100

    new yml

--- inflammation/models.py
+++ inflammation/models.py
@@ -34,7 +34,31 @@ def daily_min(data):
 
 
 class Patient:
+    """A patient in an inflammation study."""
     def __init__(self, name):
         self.name = name
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1]['day'] + 1
+
+            except IndexError:
+                day = 0
 
 
+        new_observation = {
+            'day': day,
+            'value': value,
+        }
+
+        self.observations.append(new_observation)
+        return new_observation
+
+    def __str__(self):
+        return self.name
+
+
+alice = Patient('Alice')
+print(alice)
